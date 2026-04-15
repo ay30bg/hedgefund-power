d7ed038b399f8d44f2e8e9ac990e9a362fc31639
retro.dev <117005347+ay30bg@users.noreply.github.com>
2026-04-15 18:32:31 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,65 +1,3 @@
-// import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
-// import "./App.css";
-
-// import Header from "./components/Header";
-// import BottomNavbar from "./components/BottomNavbar";
-// import ScrollToTop from "./components/ScrollToTop";
-
-// import Home from "./pages/Home";
-// import Profile from "./pages/Profile";
-// import About from "./pages/About";
-// import Market from "./pages/Market";
-// import InvestHub from "./pages/InvestHub";
-// import Rewards from "./pages/Rewards";
-// import Faq from "./pages/FAQ";
-// import Invite from "./pages/InviteFriends";
-// import Portfolio from "./pages/Portfolio"
-// import Login from "./pages/Login";
-// import Signup from "./pages/Signup"; 
-
-// function Layout() {
-//   const location = useLocation();
-
-//   // hide header and bottom nav on auth pages
-//   const isAuthPage =
-//     location.pathname === "/login" || location.pathname === "/signup";
-
-//   return (
-//     <>
-//       {!isAuthPage && <Header />}
-
-//       <main className={isAuthPage ? "login-main" : "main-content"}>
-//         <Routes>
-//           <Route path="/" element={<Home />} />
-//           <Route path="/market" element={<Market />} />
-//           <Route path="/investhub" element={<InvestHub />} />
-//           <Route path="/profile" element={<Profile />} />
-//           <Route path="/about" element={<About />} />
-//           <Route path="/rewards" element={<Rewards />} />
-//           <Route path="/faq" element={<Faq />} />
-//           <Route path="/invite" element={<Invite />} />
-//           <Route path="/portfolio" element={<Portfolio />} />
-//           <Route path="/login" element={<Login />} />
-//           <Route path="/signup" element={<Signup />} />
-//         </Routes>
-//       </main>
-
-//       {!isAuthPage && <BottomNavbar />}
-//     </>
-//   );
-// }
-
-// function App() {
-//   return (
-//     <Router>
-//       <Layout />
-//     </Router>
-//   );
-// }
-
-// export default App;
-
-
 import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import "./App.css";
 
@@ -74,6 +12,8 @@ import Market from "./pages/Market";
 import InvestHub from "./pages/InvestHub";
 import Rewards from "./pages/Rewards";
 import Faq from "./pages/FAQ";
+import TopUp from "./pages/TopUp";
+import Withdraw from "./pages/Withdraw";
 import Invite from "./pages/InviteFriends";
 import Portfolio from "./pages/Portfolio";
 import Login from "./pages/Login";
@@ -99,6 +39,8 @@ function Layout() {
           <Route path="/about" element={<About />} />
           <Route path="/rewards" element={<Rewards />} />
           <Route path="/faq" element={<Faq />} />
+          <Route path="/topup" element={<TopUp />} />
+          <Route path="/withdraw" element={<Withdraw />} />
           <Route path="/invite" element={<Invite />} />
           <Route path="/portfolio" element={<Portfolio />} />
           <Route path="/login" element={<Login />} />
