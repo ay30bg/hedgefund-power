9ea55624f43f8c4739c4c61fee3cf7eb26a4ea65
retro.dev <117005347+ay30bg@users.noreply.github.com>
2026-04-20 10:24:32 +0100

    Update Profile.jsx

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -69,6 +69,9 @@ const Profile = () => {
       <div className="skeleton menu-item"></div>
       <div className="skeleton menu-item"></div>
       <div className="skeleton menu-item"></div>
+      <div className="skeleton menu-item"></div>
+      <div className="skeleton menu-item"></div>
+      <div className="skeleton menu-item"></div>
     </div>
   );
 }
