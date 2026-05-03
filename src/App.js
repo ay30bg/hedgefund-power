0964411a822493caab975a21ed5ec79f24258a90
retro.dev <117005347+ay30bg@users.noreply.github.com>
2026-05-03 10:13:59 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ function Layout() {
   const location = useLocation();
 
   const isAuthPage =
-    ["/", "/signup", "/forgot-password"].includes(location.pathname) ||
+    ["/", "/signup", "/forgot-password", "/verify"].includes(location.pathname) ||
     location.pathname.startsWith("/reset-password");
 
   return (
