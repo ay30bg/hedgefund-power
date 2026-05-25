c964f34f88c649f937868a142c122681ebb06862
retro.dev <117005347+ay30bg@users.noreply.github.com>
2026-05-25 13:32:44 +0100

    Update PrivacyPolicy.jsx

--- src/pages/PrivacyPolicy.jsx
+++ src/pages/PrivacyPolicy.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { FiArrowLeft } from "react-icons/fi";
 import "../styles/privacyPolicy.css";
